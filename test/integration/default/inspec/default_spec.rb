da5f26f3065174ec778b385f1b2d8c03ee6bd76a
Derek Groh <dgroh@us.ibm.com>
2016-09-16 01:45:05 -0500

    Create a default_spec for integration testing
    skeleton file for future default recipe integration tests

---
+++ test/integration/default/inspec/default_spec.rb
@@ -0,0 +1,7 @@
+describe 'auto-patch::default' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+  it 'configures automatic patching' do
+    skip 'To do'
+  end
+end
